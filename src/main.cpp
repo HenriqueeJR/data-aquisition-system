6b76c690735a4e4d9fac25cd246657e3bbf0ee94
HenriqueeJR <91606600+HenriqueeJR@users.noreply.github.com>
2023-10-31 23:27:32 +0000

    Dialog is here!

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <iomanip>
 #include <sstream>
+#include <fstream>
 
 using boost::asio::ip::tcp;
 
@@ -22,6 +23,13 @@ std::time_t string_to_time_t(const std::string& time_string) {
     std::istringstream ss(time_string);
     ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S");
     return std::mktime(&tm);
+}
+
+std::string time_t_to_string(std::time_t time) {
+    std::tm* tm = std::localtime(&time);
+    std::ostringstream ss;
+    ss << std::put_time(tm, "%Y-%m-%dT%H:%M:%S");
+    return ss.str();
 }
 
 class session
@@ -50,35 +58,47 @@ private:
             std::istream is(&buffer_);
             std::string message(std::istreambuf_iterator<char>(is), {});
             std::cout << "Received: " << message << std::endl;
+            message.resize(message.size() - 2);
 
 
-            //separando a mensagem recebida em partes
-            std::string msg = "";
-            std::string id = "";
-            std::string data = "";
-            std::string leitura = "";
-            int contador_separadores = 0;
-
-            for (size_t i = 0; i < message.length(); ++i) {
-                if(message[i] != '|' && contador_separadores == 0) {
-                    msg += message[i];
-                }
-                else if(message[i] != '|' && contador_separadores == 1) {
-                    id += message[i];
-                }
-                else if(message[i] != '|' && contador_separadores == 2) {
-                    data += message[i];
-                }
-                else if(message[i] != '|' && contador_separadores == 3) {
-                    leitura += message[i];
-                }
-                else if (message[i] == '|') {
-                    contador_separadores++;
-                }
+            std::string msg_definition = "";
+            for (int i=0; i<3; ++i) {
+              msg_definition += message[i];
             }
 
+
+
+            
+
             //analisando cada mensagem
-            if (msg == "LOG") {
+            if (msg_definition == "LOG") {
+
+                //separando a mensagem recebida em partes
+                std::string msg = "";
+                std::string id = "";
+                std::string data = "";
+                std::string leitura = "";
+                int contador_separadores = 0;
+
+                for (size_t i = 0; i < message.length(); ++i) {
+                    if(message[i] != '|' && contador_separadores == 0) {
+                        msg += message[i];
+                    }
+                    else if(message[i] != '|' && contador_separadores == 1) {
+                        id += message[i];
+                    }
+                    else if(message[i] != '|' && contador_separadores == 2) {
+                        data += message[i];
+                    }
+                    else if(message[i] != '|' && contador_separadores == 3) {
+                        leitura += message[i];
+                    }
+                    else if (message[i] == '|') {
+                        contador_separadores++;
+                    }
+                }
+
+                
                 //criando o vetor de char
                 char sensor_id[32];
                 for (int i = 0; i < 32; i++) {
@@ -103,8 +123,89 @@ private:
                 rec.timestamp = time;
                 rec.value = value;
 
+
+
+                std::string filename = id;
+                filename += ".dat";
+
+                std::fstream file(filename, std::fstream::out | std::fstream::in | std::fstream::binary 
+																	 | std::fstream::app); 
+
+                if(file.is_open()) {
+                  file.write((char*)&rec, sizeof(LogRecord));
+                }
+                file.close();
+                write_message(message);
             }
-            write_message(message);
+            
+            if (msg_definition == "GET") {
+              //separando a mensagem recebida em partes
+                std::string msg = "";
+                std::string id = "";
+                std::string numb_reg = "";
+                int contador_separadores = 0;
+
+                for (size_t i = 0; i < message.length(); ++i) {
+                    if(message[i] != '|' && contador_separadores == 0) {
+                        msg += message[i];
+                    }
+                    else if(message[i] != '|' && contador_separadores == 1) {
+                        id += message[i];
+                    }
+                    else if(message[i] != '|' && contador_separadores == 2) {
+                        numb_reg += message[i];
+                    }
+                    else if (message[i] == '|') {
+                        contador_separadores++;
+                    }
+                }
+
+                int n_reg = std::stoi(numb_reg);
+                std::string filename = id;
+                filename += ".dat";
+
+                std::string response_message = "";
+                response_message += numb_reg;
+                response_message += ";";
+
+
+                std::fstream file(filename, std::fstream::out | std::fstream::in | std::fstream::binary 
+																	 | std::fstream::app); 
+
+                if(file.is_open()) {
+                  file.seekg(0, std::ios_base::end);
+                  int file_size = file.tellg();
+
+                  int n = file_size/sizeof(LogRecord);
+		              std::cout << "Num records: " << n << " (file size: " << file_size << " bytes)" << std::endl;
+
+                  for(int i = 1; i<=n_reg; ++i) {   //numero de leituras a serem feitas
+                    if(i < n) {
+                      file.seekg((-1)*(i)*sizeof(LogRecord), std::ios_base::end);
+                      LogRecord rec;
+		                  file.read((char*)&rec, sizeof(LogRecord));
+                      std::string response_time = time_t_to_string(rec.timestamp);
+                      std::string response_leitura = std::to_string(rec.value);
+
+                      response_message += response_time;
+                      response_message += "|";
+                      response_message += response_leitura;
+                      response_message += ";";
+                    }
+                    else break;
+                  }
+
+                  response_message.pop_back();   //removendo o ultimo ";"
+                  response_message += "\r\n";
+
+                  write_message(response_message);
+
+                }
+                file.close();
+            }
+            
+
+
           }
         });
   }
